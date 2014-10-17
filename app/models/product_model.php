0a18f11189d6135f156e4593811e0300c0a58bc8
pichai chin <pichaichin@pichais-MacBook-Air.local>
2014-10-17 17:32:20 +0700

    edit 1. /Users/pichaichin/Website/MYCI/app/core/MY_Model.php 2. /Users/pichaichin/Website/MYCI/app/models/product_model.php

--- app/models/product_model.php
+++ app/models/product_model.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Product_model extends CI_Model {
+class Product_model extends MY_Model {
 
 	public function __construct()
 	{
